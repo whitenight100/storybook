3780ac46b1fe609967c0127bdbbc8f3204f9e8a0
Giacomo Minighin <giacomo.minighin@gmail.com>
2023-01-22 08:57:43 +0100

    fix: fixed has polyfill

--- ext/theme-webpack-entries/lib/polyfills.ts
+++ ext/theme-webpack-entries/lib/polyfills.ts
@@ -1,28 +1,48 @@
 export function polyfillHasSelector(
-  parentSelectors: string,
-  childSelectors: string,
+  selector: string,
   style: CSSStyleDeclaration,
   mediaQueries: { [key: string]: CSSStyleDeclaration } = {}
 ) {
   // check if :has selector is supported
   if (!CSS.supports('selector(:has(a, b))')) {
-    const parentElements = Array.from(document.querySelectorAll(parentSelectors));
-    parentElements.forEach((parentElement: HTMLElement) => {
-      const childElements = Array.from(parentElement.querySelectorAll(childSelectors));
-      if (childElements.length > 0) {
-        // add css to parent element
-        Object.keys(style).forEach((key) => {
-          parentElement.style[key] = style[key];
-        });
-        // add css to parent element for each media query
-        Object.keys(mediaQueries).forEach((mediaQuery) => {
-          if (window.matchMedia(mediaQuery).matches) {
-            Object.keys(mediaQueries[mediaQuery]).forEach((key) => {
-              parentElement.style[key] = mediaQueries[mediaQuery][key];
-            });
-          }
-        });
-      }
+    const elements = has(selector);
+    elements.forEach((parentElement: HTMLElement) => {
+      // add css to the element
+      Object.keys(style).forEach((key) => {
+        parentElement.style[key] = style[key];
+      });
+      // add css to the element for each media query
+      Object.keys(mediaQueries).forEach((mediaQuery) => {
+        if (window.matchMedia(mediaQuery).matches) {
+          Object.keys(mediaQueries[mediaQuery]).forEach((key) => {
+            parentElement.style[key] = mediaQueries[mediaQuery][key];
+          });
+        }
+      });
     });
   }
+}
+
+function has(selector: string): Element[] {
+  const parts = selector.match(/(.*):has\((.*)\)(.*)/);
+  const parentSelector = parts[1].trim();
+  const childSelector = parts[2].trim();
+  const postSelector = parts[3].trim();
+
+  const matches = [];
+  const parentElements = document.querySelectorAll(parentSelector);
+  for (const parentElement of Array.from(parentElements)) {
+    const childElements = parentElement.querySelectorAll(':scope ' + childSelector);
+    if (childElements.length > 0) {
+      if (postSelector && postSelector !== '') {
+        const postElements = parentElement.querySelectorAll(':scope ' + postSelector);
+        for (const postElement of Array.from(postElements)) {
+          matches.push(postElement);
+        }
+      } else {
+        matches.push(parentElement);
+      }
+    }
+  }
+  return matches;
 }
